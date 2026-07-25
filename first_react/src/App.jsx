1aa4353538236f992c8c046b0aefdcdd826112ff
Harshita Sharma <harshitakaushish004@gmail.com>
2026-07-25 22:08:53 +0530

    Updated App component

---
+++ first_react/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React  learning journey   !</h1>
+      <p>I'm a developer with a passion for building fast, interactive web applications using React and modern tooling like Vite. Beyond frontend development, I love exploring core software architecture, algorithm design, and intelligent systems. Always curious, eager to solve complex problems, and focused on turning ideas into clean, performant applications!</p>
+    </div>
+  )
+}
+
+export default App
